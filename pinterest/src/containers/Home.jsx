46d6f739944eafa795638afdf988795b7ac7c6d1
VanesaGarro <vanesagarro97@gmail.com>
2020-04-24 13:17:04 -0500

    add search function

--- pinterest/src/containers/Home.jsx
+++ pinterest/src/containers/Home.jsx
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import GetCollection from '../controller/GetCollection';
+import SearchCollection from '../controller/SearchCollection';
 import Menu from '../components/Menu';
 
 const Home = () => {
   const [dataCollection, setDataCollecion] = useState([]);
+  const [category, setcategory] = useState([]);
+
   const getCollection = () => GetCollection().then((res) => (setDataCollecion(res)));
 
   useEffect(() => {
     getCollection();
   }, []);
-
-  console.log(dataCollection);
+  const getCategory = (e) => {
+    setcategory(e.target.value);
+  };
+  console.log(category);
+  const search = () => SearchCollection(category).then((res) => (setDataCollecion(res.results)));
+  const viewCollection = () => dataCollection.map((res) => (
+    <img src={res.urls.small} alt="collection" className="card-image" />
+  ));
   return (
     <>
-      <Menu />
-        <div className="container-cards">
-          {dataCollection.map((res) => (
-            <img src={res.urls.small} alt="collection" className="card-image" />
-          ))}
-        </div>
+      <Menu
+        getCollection={getCollection}
+        search={search}
+        getCategory={getCategory}
+        category={category}
+      />
+
+      <div className="container-cards">
+        {viewCollection()}
+      </div>
+
     </>
   );
 };
