749c1bcd4a23276794374443e3d576e9b368c031
VanesaGarro <vanesagarro97@gmail.com>
2020-04-24 13:17:54 -0500

    add getCategory function

--- pinterest/src/components/Menu.jsx
+++ pinterest/src/components/Menu.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Menu.css';
 import logo from '../assets/images/pinterest.svg';
 // import PropTypes from 'prop-types';
 
-const Menu = (
-) => (
+const Menu = ({ search, getCategory, category, getCollection }) => (
   <nav className="nav-header">
 
     <li>
       <img src={logo} alt="logo" className="logo-header" />
     </li>
     <li>
-      <button type="submit" className="button-inicio">
+      <button type="submit" className="button-inicio" onClick={getCollection}>
         {' '}
         Inicio
       </button>
     </li>
 
     <div className="search">
-      <form action="#">
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        search();
+      }}
+      >
         <input
           type="text"
           placeholder="Buscar"
           name="search"
+          value={category}
+          onChange={getCategory}
         />
       </form>
     </div>
