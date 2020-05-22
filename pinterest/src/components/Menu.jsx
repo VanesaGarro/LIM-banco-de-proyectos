090249bc0a8033cc4f9001f4934594f4d7360750
VanesaGarro <vanesagarro97@gmail.com>
2020-05-22 07:13:14 -0500

    add get category function

--- pinterest/src/components/Menu.jsx
+++ pinterest/src/components/Menu.jsx
@@ -4,14 +4,23 @@ import './Menu.css';
 import logo from '../assets/images/pinterest.svg';
 // import PropTypes from 'prop-types';
 
-const Menu = ({ search, getCategory, category, getCollection }) => (
+const Menu = ({
+  search, getCategory, setcategory,
+}) => (
   <nav className="nav-header">
 
     <li>
       <img src={logo} alt="logo" className="logo-header" />
     </li>
     <li>
-      <button type="submit" className="button-inicio" onClick={getCollection}>
+      <button
+        type="submit"
+        className="button-inicio"
+        onClick={() => {
+          setcategory('aesthetic');
+          document.getElementById('searchbar').value = '';
+        }}
+      >
         {' '}
         Inicio
       </button>
@@ -27,8 +36,8 @@ const Menu = ({ search, getCategory, category, getCollection }) => (
           type="text"
           placeholder="Buscar"
           name="search"
-          value={category}
-          onChange={getCategory}
+          onKeyPress={getCategory}
+          id="searchbar"
         />
       </form>
     </div>
