85444745ba253176552c5fc21c115c6b3101b44d
VanesaGarro <vanesagarro97@gmail.com>
2020-05-22 07:10:03 -0500

    fix Search function

--- pinterest/src/controller/SearchCollection.js
+++ pinterest/src/controller/SearchCollection.js
@@ -1,11 +1,12 @@
 const clientID = 'd2mtAfA49OpMr-XGxS-8jyfpUL1Qpm6uaQ3fXNZpZzQ';
-const SearchCollection = (query) => fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${query}&client_id=${clientID}`, {
+const SearchCollection = (page, query) => fetch(`https://api.unsplash.com/search/photos?&page=${page}&query=${query}&client_id=${clientID}&per_page=30`, {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
   },
 }).then((res) => {
   if (res.status === 200) {
+    console.log(query);
     return res.json();
   }
   return Promise.reject(console.log(res));
