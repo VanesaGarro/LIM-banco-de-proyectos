28fa2556dad0b6325cf969d9733b2d407431cfa7
VanesaGarro <vanesagarro97@gmail.com>
2020-06-17 23:50:35 -0500

    Updates

---
+++ precache-manifest.c105297b539c5c92fa96641fbf11c9c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89190a4e2c4f0d915a550e86b0998bd8",
+    "url": "/LIM-banco-de-proyectos/index.html"
+  },
+  {
+    "revision": "98cd95196e0be3f829b1",
+    "url": "/LIM-banco-de-proyectos/static/css/2.9de105d4.chunk.css"
+  },
+  {
+    "revision": "04a5a40e34e09fd53317",
+    "url": "/LIM-banco-de-proyectos/static/css/main.610f77c8.chunk.css"
+  },
+  {
+    "revision": "98cd95196e0be3f829b1",
+    "url": "/LIM-banco-de-proyectos/static/js/2.ef025648.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/LIM-banco-de-proyectos/static/js/2.ef025648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a5a40e34e09fd53317",
+    "url": "/LIM-banco-de-proyectos/static/js/main.544ac799.chunk.js"
+  },
+  {
+    "revision": "919f1646fb8077801ae4",
+    "url": "/LIM-banco-de-proyectos/static/js/runtime-main.7a83856d.js"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/LIM-banco-de-proyectos/static/media/Segoe UI Bold.65099f98.ttf"
+  },
+  {
+    "revision": "3f145989d793d995e22f6ac038e3efd3",
+    "url": "/LIM-banco-de-proyectos/static/media/download.3f145989.svg"
+  },
+  {
+    "revision": "2d6e1dd6cbd3b9c66e5247f0d4c6298c",
+    "url": "/LIM-banco-de-proyectos/static/media/heart.2d6e1dd6.svg"
+  },
+  {
+    "revision": "098da3c0af5ce19ad356b368d9aa35a6",
+    "url": "/LIM-banco-de-proyectos/static/media/pinterest.098da3c0.svg"
+  },
+  {
+    "revision": "633a75e3f422fadb317394b0115af1db",
+    "url": "/LIM-banco-de-proyectos/static/media/research.633a75e3.svg"
+  }
+]);
